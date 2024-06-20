6ae3c89d25a6fe30ac82675666c481b9ec8da2ca
chenyc <flyforest@outlook.com>
2024-06-20 16:22:45 +0800

    fix: fix no blobBaseFee

--- core/evm.go
+++ core/evm.go
@@ -24,6 +24,7 @@ import (
 	"github.com/ethereum/go-ethereum/consensus/misc/eip4844"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/holiman/uint256"
 )
 
@@ -57,6 +58,9 @@ func NewEVMBlockContext(header *types.Header, chain ChainContext, author *common
 	}
 	if header.ExcessBlobGas != nil {
 		blobBaseFee = eip4844.CalcBlobFee(*header.ExcessBlobGas)
+	}
+	if blobBaseFee == nil {
+		blobBaseFee = new(big.Int).SetUint64(params.BlobTxMinBlobGasprice)
 	}
 	if header.Difficulty.Cmp(common.Big0) == 0 {
 		random = &header.MixDigest
